2cb71340496e0f6feabba2832d006a34cb3bbedd
Miller777 <miller777@mail.ru>
2021-06-21 02:10:36 +0300

    first commit

---
+++ lib/pages/register_form_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class RegisterFormPage extends StatefulWidget {
+  @override
+  _RegisterFormPageState createState() => _RegisterFormPageState();
+}
+
+class _RegisterFormPageState extends State<RegisterFormPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Register Form'),
+        centerTitle: true,
+      ),
+      body: Form(
+        child: ListView(
+          padding: EdgeInsets.all(16.0),
+        ),
+      ),
+    );
+  }
+}
